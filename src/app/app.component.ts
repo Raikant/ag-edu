3820a31133ab8ec5a66b495e7bce1c396259aaca
Ravi_Pathak <ravi.pathak918@gmail.com>
2024-12-04 03:26:48 -0500

    added left panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from './footer/footer.component';
+import {LeftpanelComponent} from './leftpanel/leftpanel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, LeftpanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ag-edu';
+  title = 'welcome';
 }
---
+++ src/app/leftpanel/leftpanel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leftpanel',
+  imports: [],
+  templateUrl: './leftpanel.component.html',
+  styleUrl: './leftpanel.component.css'
+})
+export class LeftpanelComponent {
+
+}
